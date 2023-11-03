ef815f9a0f19210df55307a078c8807518196727
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-11-04 00:23:04 +0100

    initial View module

---
+++ View/base.py
@@ -0,0 +1,21 @@
+import sys
+
+LOGIN_MENU = """
+1 - Connexion
+2 - Inscription
+3 - Quitter
+"""
+
+CRUD_MENU = """
+1 - Ajouter
+2 - Modifier
+3 - Supprimer
+4 - Retour
+"""
+
+GENERAL_MENU = """
+Recherchez :
+1 - Un (des) client(s)
+2 - Un (des) contrat(s)
+3 - Un (des) évènements(s)
+"""
