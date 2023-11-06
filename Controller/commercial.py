d5b985432d91d5c497da3888368c72dca85c18be
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-11-06 23:58:59 +0100

    Commercial department controller file

---
+++ Controller/commercial.py
@@ -0,0 +1,19 @@
+from Model.base import new_entry, get_entry, get_all, quitter
+from View.base import affichage_menu, get_object_id
+from View.commercial import COMMERCIAL_MENU
+
+def commercial_controller():
+	options = {"1": "clients", "2": "gestion-contrats", "3": "affichage-contrats", "4": "evenements"}
+	menu_choisi = affichage_menu(COMMERCIAL_MENU, options)
+	if menu_choisi == "clients":
+		id_utilisateur = get_object_id()
+		# id_utilisateur = get_user_id()
+		profil_utilisateur = new_entry("collaborateurs", id_utilisateur)
+	elif menu_choisi == "gestion-contrats":
+		pass
+	elif menu_choisi == "affichage-contrats":
+		quitter()
+	elif menu_choisi == "evenements":
+		quitter()
+	else :
+		return 0
