8cf3fd7e4febe3f6baa50963095c50c0913807fa
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-11-07 23:50:46 +0100

    Model package - base file

---
+++ Model/base.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+con = sqlite3.connect("database.db")
+cur = con.cursor()
+
+def new_entry(table, data):
+		new_row = tuple(data)
+		command = """INSERT INTO {table} VALUES(?)"""
+		cur.executemany(command, new_row)
+		id = cur.lastrowid
+		new = cur.lastrow
+		con.commit()
+		# print('dernier id: %d' % id)
+		print('nouvelle ligne : %d' % new)
+		return cur.lastrow
+
+
+def get_entry(table, id):
+		command = f"""SELECT * FROM {table} WHERE {id}"""
+		query = cur.execute(command)
+		return query.fetchone()
+
+def get_all(table):
+		command = f"""SELECT * FROM {table}"""
+		query = cur.execute(command)
+		return query.fetchall()
+		
+def update(table, id, data):
+		command = f"UPDATE {table} SET {data} WHERE id={id}"
+		cur.execute(command)
+
+def delete(table, id):
+		command = f"DELETE {table} WHERE id={id}"
+		cur.execute(command)
+
+def filter_table(table, columns=None, conditions=None):
+		# command = f""SELECT {columns if columns else '*'} FROM {table}""
+		if conditions :
+			command = command + f"WHERE {conditions}"
+
+		query = cur.execute(command)
+		return query.fetchall()
