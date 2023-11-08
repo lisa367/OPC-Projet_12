7e44f94a9b2a819aa546736859170917b147d159
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-11-08 01:52:51 +0100

    filter_table function updated

--- Model/base.py
+++ Model/base.py
@@ -4,26 +4,27 @@ con = sqlite3.connect("database.db")
 cur = con.cursor()
 
 def new_entry(table, data):
-		new_row = tuple(data)
-		command = """INSERT INTO {table} VALUES(?)"""
-		cur.executemany(command, new_row)
-		id = cur.lastrowid
-		# new = cur.lastrow
-		con.commit()
-		print('dernier id: %d' % id)
-		# print('nouvelle ligne : %d' % new)
-		return id
+	new_row = tuple(data)
+	command = """INSERT INTO {table} VALUES(?)"""
+	cur.executemany(command, new_row)
+	id = cur.lastrowid
+	# new = cur.lastrow
+	con.commit()
+	print('dernier id: %d' % id)
+	# print('nouvelle ligne : %d' % new)
+
+	return id
 
 
 def get_entry(table, id):
-		command = f"""SELECT * FROM {table} WHERE {id}"""
-		query = cur.execute(command)
-		return query.fetchone()
+	command = f"""SELECT * FROM {table} WHERE {id}"""
+	query = cur.execute(command)
+	return query.fetchone()
 
 def get_all(table):
-		command = f"""SELECT * FROM {table}"""
-		query = cur.execute(command)
-		return query.fetchall()
+	command = f"""SELECT * FROM {table}"""
+	query = cur.execute(command)
+	return query.fetchall()
 		
 def update(table, id, data):
 	command = f"UPDATE {table} SET {data} WHERE id={id}"
@@ -32,8 +33,23 @@ def update(table, id, data):
 def delete(table, id):
 	command = f"DELETE {table} WHERE id={id}"
 	cur.execute(command)
+	
 
 def filter_table(table, columns=None, conditions=None):
+		columns_names = columns if columns else "*"
+		command = f"SELECT {columns_names} FROM {table}"
+		# command = f""select {columns if columns else '*'} from {table}""
+		if conditions :
+			command = command + f"WHERE {conditions}"
+
+		query = cur.execute(command)
+		return query.fetchall()
+
+    
+con.close()
+
+
+""" def filter_table(table, columns=None, conditions=None):
 	# command = f""SELECT {columns if columns else '*'} FROM {table}""
     if columns:
         # command = f""SELECT {columns} FROM {table}""
@@ -41,10 +57,10 @@ def filter_table(table, columns=None, conditions=None):
 	else :
 		# command = f""SELECT * FROM {table}""
         command = ''	
+
 	if conditions :
 		command = command + f"WHERE {conditions}"
-	query = cur.execute(command)
+		query = cur.execute(command)
+		
 	return query.fetchall()
-
-
-con.close()
+ """
