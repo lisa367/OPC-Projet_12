ff4579c7bf7ef3c9de0134ffcbb980ab4f3e8ce7
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-11-13 04:11:40 +0100

    db model for contrats

---
+++ Model/contrats.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+con = sqlite3.connect("database.db")
+cur = con.cursor()
+
+contrats_colonnes = ["prenom", "nom", "departement", "role", "email",]
+
+
+def create_table():
+		# command = f"""CREATE TABLE IF NOT EXISTS {employees}(
+					# title, year, score)"""
+		cur.execute("""CREATE TABLE IF NOT EXISTS contrats(
+					client_id INTEGER FOREIGN KEY, 
+					contact_commercial TEXT,
+					montant_facture REAL, 
+					montant_redevable TEXT,
+					date_creation TEXT,
+                    statut TEXT DEFAULT 'non-signé'
+)
+""")
+
+con.commit()
