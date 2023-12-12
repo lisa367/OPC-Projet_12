92ab11902a50cdd3f2929ecd3113d3fa0ff26ce5
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-12-12 01:46:58 +0100

    modele pour les collaborateurs

---
+++ Model/collaborateurs.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+con = sqlite3.connect("database.db")
+cur = con.cursor()
+
+collaborateurs_colonnes = ["prenom", "nom", "departement", "role", "email",]
+
+
+def create_table():
+		# command = f"""CREATE TABLE IF NOT EXISTS {employees}(
+					# title, year, score)"""
+		cur.execute("""CREATE TABLE IF NOT EXISTS collaborateurs(
+					prenom TEXT, 
+					nom TEXT,
+					departement TEXT, 
+					role TEXT,
+					email TEXT
+)
+""")
+
+con.commit()
