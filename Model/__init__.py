0272fd5ed2a75c272112e18e0c5e1da57bfeee56
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-12-12 04:28:57 +0100

    creating tables from Model module import

---
+++ Model/__init__.py
@@ -0,0 +1,10 @@
+from clients import create_table_clients
+from collaborateurs import create_table_collaborateurs
+from contrats import create_table_contrats
+from evenements import create_table_evenements
+
+
+create_table_clients()
+create_table_collaborateurs()
+create_table_contrats()
+create_table_evenements()
