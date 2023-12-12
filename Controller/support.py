5d244379ebf50a9bcf0f5c448799836786c1c56d
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-12-12 19:47:27 +0100

    updated Support controller

--- Controller/support.py
+++ Controller/support.py
@@ -1,16 +1,21 @@
-from Model.base import new_entry, get_entry, get_all
-from View.base import affichage_menu, get_object_id
+from Model.base import new_entry, filter_table, update, get_entry, get_all
+from Model.evenements import evenements_colonnes
+from View.base import affichage_menu, get_object_id, get_query_filters, get_object_data, add_or_modify
 from View.support import SUPPORT_MENU
 
 def support_controller():
-	options = {"1": "gestion-evenements", "2": "affichage-evenements",}
+	options = {"1": "affichage-evenements", "2": "gestion-evenements",}
 	menu_choisi = affichage_menu(SUPPORT_MENU, options)
 
-	if menu_choisi == "gestion-evenements":
-		id_utilisateur = get_object_id()
-		profil_utilisateur = new_entry("collaborateurs", id_utilisateur)
-	
-	elif menu_choisi == "affichage-evenements":
-		pass
+	if menu_choisi == "affichage-evenements":
+		columns, conditions = get_query_filters(evenements_colonnes)
+		query_result = filter_table("evenements", columns, conditions)
+		print(query_result)
+
+	elif menu_choisi == "gestion-evenements":
+		id_evenement = get_object_id()
+		data_evenement = get_object_data("evenement", evenements_colonnes)
+		update("evenements", id_evenement, data_evenement)
+
 	else :
 		return 0
