13b0c9326110eeabd86db669c819276cb3ae0c34
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-12-13 19:43:10 +0100

    updated Model __init__ file to create admin profile with db instantiation

--- Model/__init__.py
+++ Model/__init__.py
@@ -1,10 +1,18 @@
 from .clients import create_table_clients
-from .collaborateurs import create_table_collaborateurs
+from .collaborateurs import create_table_collaborateurs, collaborateurs_colonnes
 from .contrats import create_table_contrats
 from .evenements import create_table_evenements
+from .base import new_entry, new_entries, get_all
 
 
 create_table_collaborateurs()
 create_table_clients()
 create_table_contrats()
 create_table_evenements()
+
+# print(get_all("collaborateurs"))
+collaborateurs_db_content = get_all("collaborateurs")
+
+if not collaborateurs_db_content:
+    admin_data = ["admin", "admin", "Gestion", "administrateur", "admin@fauxmail.com"]
+    new_entry("collaborateurs", collaborateurs_colonnes, admin_data)
