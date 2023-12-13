abd0d9cb118e8c92603560df3a736610c2cd47d5
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-12-13 18:00:58 +0100

    updated operations_crud and other crud related functions

--- Controller/base.py
+++ Controller/base.py
@@ -1,9 +1,10 @@
+import os
 import sys
 import sqlite3
 
 from Model.base import new_entry, get_entry, get_all, update, delete, filter_table
 from Model.collaborateurs import collaborateurs_colonnes
-from View.base import LOGIN_MENU, CRUD_MENU , affichage_menu, get_user_id, quitter, get_object_id, get_object_data
+from View.base import LOGIN_MENU, CRUD_MENU , affichage_menu, get_user_id, get_object_id, get_object_data
 
 
 con = sqlite3.connect("database.db")
@@ -16,7 +17,7 @@ def quitter():
 
 def connexion():
 	id_utilisateur = get_user_id()
-	profil_utilisateur = new_entry("collaborateurs", id_utilisateur)
+	profil_utilisateur = get_entry("collaborateurs", id_utilisateur)
 	return profil_utilisateur
 
 
@@ -24,7 +25,7 @@ def inscription():
 	data_utilisateur = get_object_data("collaborateur", collaborateurs_colonnes)
 	""" if get_all("collaborateurs" == []):
 		data_utilisateur = [("rowid", 1)] + data_utilisateur """
-	profil_utilisateur = get_entry("collaborateurs", data_utilisateur)
+	profil_utilisateur = new_entry("collaborateurs", collaborateurs_colonnes, data_utilisateur)
 	return profil_utilisateur
 
 
@@ -32,19 +33,43 @@ def login():
 	options = {"1": "connexion", "2": "inscription", "3": "quitter"}
 	menu_choisi = affichage_menu(LOGIN_MENU, options)
 	if menu_choisi == "connexion":
-		connexion()
+		profil_utilisateur = connexion()
+		return profil_utilisateur
 	elif menu_choisi == "inscription":
-		inscription()
+		profil_utilisateur = inscription()
+		return profil_utilisateur
 	elif menu_choisi == "quitter":
 		quitter()
 	else :
 		return 0
 
 def accueil():
-	login_page = login()
-	while login_page == 0:
-		print("Veuillez saisir une entrée valide")
-		login()
+	show_login_page = True
+	while show_login_page == True:
+		profile_valide = login()
+		if profile_valide :
+			show_login_page = False
+			return profile_valide
+		else :
+			print("Veuillez saisir une entrée valide")
+		# login()
+
+def ajouter(object_name, table_name, table_columns):
+	raw_data = get_object_data(object_name, table_columns)
+	data_list = [data[1] for data in raw_data]
+	data_string = ", ".join(data_list)
+	table_columns_string = ', '.join(table_columns)
+	new_entry(table_name, table_columns_string, data_string)
+
+
+def modifier(object_name, table_name, table_columns):
+	object_id = get_object_id(object_name)
+	print("Renseignez les nouvelles valeurs pour les colonnes à modifier ou tapez sur Entrée")
+	raw_data = get_object_data(object_name, table_columns)
+	data_list = [f"{data[0]} = {data[1]}" for data in raw_data if data[1]]
+	data_string = ", ".join(data_list)
+	update(table_name, object_id, data_string)
+
 
 def operations_crud(object_name, table_name, table_columns):
 	print(f"Ajouter, modifier, ou supprimer un {object_name}")
@@ -54,20 +79,28 @@ def operations_crud(object_name, table_name, table_columns):
 	# while option_choisie != "retour" :
 
 	if option_choisie == "ajouter":
-		object_data = get_object_data(object_name, table_columns)
-		new_entry(table_name, object_name)
+		ajouter(object_name, table_name, table_columns)
+		""" raw_data = get_object_data(object_name, table_columns)
+		data_list = [data[1] for data in raw_data]
+		data_string = ", ".join(data_list)
+		table_columns_string = ', '.join(table_columns)
+		new_entry(table_name, table_columns_string, data_string) """
 
 	elif option_choisie == "modifier":
-		object_id = get_object_id(object_name)
-		object_data = get_object_data(table_name, object_name)
-		update(table_name, object_id, object_data)
+		modifier(object_name, table_name, table_columns)
+		""" object_id = get_object_id(object_name)
+		print("Renseignez les nouvelles valeurs pour les colonnes à modifier ou tapez sur Entrée")
+		raw_data = get_object_data(object_name, table_columns)
+		data_list = [f"{data[0]} = {data[1]}" for data in raw_data if data[1]]
+		data_string = ", ".join(data_list)
+		update(table_name, object_id, data_string) """
 
 	elif option_choisie == "supprimer":
 		object_id = get_object_id(object_name)
 		delete(table_name, object_id)
 
 	elif option_choisie == "retour":
-		pass
-	
+		return "retour"
+
 	else :
 		return 0
