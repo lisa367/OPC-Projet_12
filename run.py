14662cdd02c6be475f860018330523ddce414475
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-12-13 17:31:55 +0100

    included sentry configuration in run file

--- run.py
+++ run.py
@@ -1,21 +1,29 @@
+import sentry_sdk
 from Controller.base import accueil
 from Controller.gestion import gestion_controller
 from Controller.commercial import commercial_controller
 from Controller.support import support_controller
 
+
 def menu_selon_departement(profil_utilisateur):
-	if profil_utilisateur(3) == "gestion":
+	if profil_utilisateur[3] == "gestion":
 		gestion_controller()
-	elif profil_utilisateur(3) == "commercial":
+	elif profil_utilisateur[3] == "commercial":
 		commercial_controller()
-	elif profil_utilisateur(3) == "support":
+	elif profil_utilisateur[3] == "support":
 		support_controller()
 	else:
 		return 0
 
 
 def run_program():
-	pass
+	sentry_sdk.init(
+    dsn="https://a09900948fc542f2e4474a0f0e5e66a4@o4506244442554368.ingest.sentry.io/4506377879289856",
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+)
+	# division_by_zero = 1 / 0
+	accueil()
 
 if __name__ == "__main__":
 	run_program()
