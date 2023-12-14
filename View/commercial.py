6a7f41599688ab7d6561af736b6d3c9adf561f0c
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-12-14 07:04:26 +0100

    updated view for commercial department

--- View/commercial.py
+++ View/commercial.py
@@ -1,6 +1,8 @@
 COMMERCIAL_MENU = """
-1 - Gestion de ses clients
+1 - Gestion des clients
 2 - Gestion des contrats de ses clients
-3 - Filtrer affichage des contrats
+3 - Filtrer l'affichage des contrats
 4 - Créer un évènement pour un nouveau contrat signé
+6 - Quitter
+
 """
